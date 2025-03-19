1753dd7c7f2d7d335acfb9f32ff7bcfb7c384d0f
Mohamed Khaled <mohamedkhaledtharwat123@gmail.com>
2025-03-19 16:54:33 +0200

    h

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import DateCounter from "./DateCounter"
+import { useEffect, useReducer } from 'react'
+import Header from './Header'
+import Main from './Main'
 
+const initialState={
+  questions:[],
+  status : "loading",
+}
+function reducer(state ,action){
+  switch(action.type){
+    case "fetchQuestion": 
+      return {...state ,questions: action.payload  ,status : "ready"};
+    case "failedFetch":
+      return {...state , status :"failed"}
+    default : throw new Error("error");
+  }
+}
 function App() {
+  const [state ,dispatch] = useReducer(reducer,initialState);
+  console.log(state)
+  useEffect(()=>{
+    try {
+      fetch("http://localhost:9000/questions")
+      .then((res)=>res.json())
+      .then((data)=>dispatch({type:"fetchQuestion" , payload : data}))
+    } catch (error) {
+      dispatch({type:"failedFetch" ,payload: error })
+    }
+  },[])
   return (
-    <div>
-      <DateCounter />
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   )
 }
